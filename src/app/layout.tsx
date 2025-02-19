5f4f4942701a7ebf67bc0357056664acb0fdf6c0
Sérgio Dantas <sergiodnts828@gmail.com>
2025-02-19 01:04:06 -0300

    feat: create home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,9 @@ export default function RootLayout({
       lang="en"
       className={`${oxanium.variable} ${montserrat.variable}`}
     >
-      <body className="bg-gray-900 text-gray-100 antialiased">{children}</body>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top">
+        {children}
+      </body>
     </html>
   )
 }
